9e4365a81cf425c06ea09037af047f76009c4b6c
randriamiharyDani <randriamiharySteev@gmail.com>
2025-09-03 15:21:38 +0300

    ui : create compposant produit

--- resources/facture-front/src/app/pages/produits/produits.component.ts
+++ resources/facture-front/src/app/pages/produits/produits.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-produits',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.scss'
 })
 export class ProduitsComponent {
+  users = [
+  {
+    name: 'James Butt',
+    country: 'Algeria',
+    flag: 'https://flagcdn.com/dz.svg',
+    joinDate: '01/15/2023',
+    createdBy: 'Iori Bowcher',
+    activity: 80
+  },
+  {
+    name: 'Josephine Darakjy',
+    country: 'Panama',
+    flag: 'https://flagcdn.com/pa.svg',
+    joinDate: '03/22/2023',
+    createdBy: 'Amy Eisner',
+    activity: 65
+  },
+  {
+    name: 'Art Venere',
+    country: 'Slovenia',
+    flag: 'https://flagcdn.com/si.svg',
+    joinDate: '06/10/2023',
+    createdBy: 'Iori Bowcher',
+    activity: 90
+  }
+];
+
 
 }
