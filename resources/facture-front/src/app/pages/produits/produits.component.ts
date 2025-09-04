c00b8195fd9054d3a0594edb4b807022699f472a
randriamiharyDani <randriamiharySteev@gmail.com>
2025-09-04 11:27:48 +0300

    ui :rectifier modal product

--- resources/facture-front/src/app/pages/produits/produits.component.ts
+++ resources/facture-front/src/app/pages/produits/produits.component.ts
@@ -14,50 +14,14 @@ export class ProduitsComponent {
   isModalOpen = false;
   users = [
     {
-      name: 'James Butt',
-      country: 'Algeria',
-      flag: 'https://flagcdn.com/dz.svg',
-      joinDate: '01/15/2023',
-      createdBy: 'Iori Bowcher',
+      nom: 'bolo',
+      prix: '200',
+      tva: '20',
+      stock: '600',
+      description: 'Iori Bowcher',
       activity: 80
     },
-    {
-      name: 'Josephine Darakjy',
-      country: 'Panama',
-      flag: 'https://flagcdn.com/pa.svg',
-      joinDate: '03/22/2023',
-      createdBy: 'Amy Eisner',
-      activity: 65
-    },
-    {
-      name: 'Art Venere',
-      country: 'Slovenia',
-      flag: 'https://flagcdn.com/si.svg',
-      joinDate: '06/10/2023',
-      createdBy: 'Iori Bowcher',
-      activity: 90
-    }, {
-      name: 'Francois',
-      country: 'Slovenia',
-      flag: 'https://flagcdn.com/si.svg',
-      joinDate: '06/10/2023',
-      createdBy: 'Iori Bowcher',
-      activity: 20
-    }, {
-      name: 'Belagny',
-      country: 'Slovenia',
-      flag: 'https://flagcdn.com/si.svg',
-      joinDate: '06/10/2023',
-      createdBy: 'Iori Bowcher',
-      activity: 55
-    }, {
-      name: 'Rapierre',
-      country: 'Slovenia',
-      flag: 'https://flagcdn.com/si.svg',
-      joinDate: '06/10/2023',
-      createdBy: 'Iori Bowcher',
-      activity: 100
-    }
+
   ];
 
   openModal() {
